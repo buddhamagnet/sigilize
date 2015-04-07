a53749528904897cb6372db32fd7910e0990727c
buddhamagnet <buddhamagnet@gmail.com>
2015-04-07 13:28:04 +0100

    initial commit

---
+++ sigilize.go
@@ -0,0 +1,24 @@
+package sigilize
+
+func removeVowels(text []string) string {
+	r := regexp.MustCompile("[aeiou]")
+	return r.ReplaceAllString(strings.Join(text, ""), "")
+}
+
+func removeRepeatedChars(text string) string {
+	chars := map[string]bool{}
+
+	for _, c := range text {
+		char := string(c)
+		if _, found := chars[char]; found {
+			r := regexp.MustCompile(char)
+			text = r.ReplaceAllString(text, "")
+		}
+		chars[char] = true
+	}
+	return text
+}
+
+func Sigilize(text []string) string {
+	return removeRepeatedChars(removeVowels(text))
+}
